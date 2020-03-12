f6eb8aad71ddda5aa57bdd8841bc67982551b608
Winnie207 <57951904+Winnie207@users.noreply.github.com>
2020-03-12 23:12:03 +0900

    masterと同期

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   get '/' => 'home#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/about' => 'home#about'
+  get '/signin' => 'home#signin'
+  get '/signup' => 'home#signup'
+  get '/main' => 'home#main'
+  get '/record' => 'home#record'
   get '/timeline' => 'home#timeline'
 end
