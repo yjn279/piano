b4f17af3de746010ce8388684b9906b124b78ed4
AKogi0718 <51438300+AKogi0718@users.noreply.github.com>
2020-03-13 14:10:48 +0900

    今のやつ(progate3講座まで反映)
    簡易投稿機能

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,17 @@
+class HomeController < ApplicationController
+  def top
+  end
+  def about
+  end
+  def record
+  end
+  def testtop
+      end
+  def main
+      end
+  def signin
+      end
+  def signup
+      end
+  
+end
---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  get 'posts/index' => "posts#index"
+  get "posts/new" => "posts#new"
+  get "posts/:id" => "posts#show"
+  post "posts/create" => "posts#create"
+  get "posts/:id/edit" => "posts#edit"
+  get 'top' =>'home#top'
+  get 'about' =>'home#about'
+  get 'record' =>'home#record'
+  get 'testtop' => 'home#testtop'
+  get 'main' => 'home#main'
+  get 'signin' => 'home#signin'
+  get 'signup' => 'home#signup'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
