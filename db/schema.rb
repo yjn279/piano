019cacbcac5904c5139b29e37093fa505f962576
Winnie207 <57951904+Winnie207@users.noreply.github.com>
2020-03-31 23:38:37 +0900

    軽微な修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_31_075958) do
+ActiveRecord::Schema.define(version: 2020_03_30_072636) do
 
   create_table "kirkokus", force: :cascade do |t|
     t.string "datetime"
