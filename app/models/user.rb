8390c7ffb0b1f204bd81ff49b74a0f9de153b79e
AKogi0718 <51438300+AKogi0718@users.noreply.github.com>
2020-04-05 22:42:33 +0900

    投稿のユーザー識別

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ApplicationRecord
     validates :email , {presence: true,uniqueness: true}
     
     validates :password, {presence: true}
+    
+    def posts
+      return Post.where(user_id: self.id)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_31_075958) do
+ActiveRecord::Schema.define(version: 2020_04_05_113126) do
 
   create_table "kirkokus", force: :cascade do |t|
-    t.string "datetime"
-    t.string "time"
+    t.datetime "datetime"
+    t.time "time"
     t.text "content"
     t.string "image"
     t.datetime "created_at", null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_03_31_075958) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "records", force: :cascade do |t|
